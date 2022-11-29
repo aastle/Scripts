7dc44e9d6f479e6e387e1b0a27eec52e3f9c4fb5
Alan Astle <aastle@gmail.com>
2022-11-29 12:51:37 -0500

    Add files via upload

---
+++ Collecting.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class Collecting : MonoBehaviour
+{
+    
+    public ItemScriptableObject item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("Item Collided with " + collision.gameObject.tag);
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        Collect(this.tag);
+    //    }
+    //}
+
+    private void Collect(ControllerColliderHit hit)
+    {
+
+        Debug.Log("Collected " + name + " " + item.ItemName);
+    }
+
+
+}
